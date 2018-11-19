564b123b8eec6254168c4fc7b46a463d411940cc
ponpppher <genmaier@gmail.com>
2018-11-20 04:16:26 +0900

    新規作成、編集、削除機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,36 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to root_path
+  end
+
   def edit
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
   def show
     @task = Task.find(params[:id])
+    render :show
   end
+
+  def destroy
+    @task = Task.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
