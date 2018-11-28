099a566033d281705c630887b68ea731c2640b96
ponpppher <genmaier@gmail.com>
2018-11-28 12:09:13 +0900

    add logout function and session's link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
