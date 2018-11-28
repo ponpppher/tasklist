8f93e81ccbfb4e1700427961d7db69d757d16053
ponpppher <genmaier@gmail.com>
2018-11-29 02:22:19 +0900

    add user test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   private
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user, class: User do
+    name {'test'}
+    email {'test@gmail.com'}
+    password {'aaaaaa'}
+    password_confirmation {'aaaaaa'}
   end
 end
