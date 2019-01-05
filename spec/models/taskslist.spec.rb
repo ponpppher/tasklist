512dbcf46a397c2ea465d2b4a6beb3fe376adc6b
ponpppher <genmaier@gmail.com>
2019-01-05 19:32:06 +0900

    add rspec test in circle ci conf

--- spec/models/taskslist.spec.rb
+++ spec/models/taskslist.spec.rb
@@ -4,10 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   before do
-    Task.create(title: 'not_yet_started_title', content: 'testcontent', status: '未着手', limit_datetime: '20181130')
-    Task.create(title: 'start_title', content: 'testcontent', status: '着手', limit_datetime: '20181201')
-    Task.create(title: 'complete_title', content: 'testcontent', status: '完了', limit_datetime: '20181215')
-    Task.create(title: 'three', content: 'testcontent', limit_datetime: '20181220')
+    User.create(name: 'first_user', email: 'a@gmail.com', password_digest: 'aaaaaa')
+    User.create(name: 'second_user', email: 'b@gmail.com', password_digest: 'aaaaaa')
+    User.create(name: 'third_user', email: 'c@gmail.com', password_digest: 'aaaaaa')
+
+    Task.create(title: 'first_task', content: 'testcontent', status: :waiting, limit_datetime: '20181130', user_id: 1)
+    Task.create(title: 'second_task', content: 'testcontent', status: :working, limit_datetime: '20181201', user_id: 2)
+    Task.create(title: 'third_task', content: 'testcontent', status: :complated, limit_datetime: '20181215', user_id: 3)
+    Task.create(title: 'fourth_task', content: 'testcontent', limit_datetime: '20181220', user_id: 1)
   end
 
   it 'titleが空であればバリデーションが通らない' do
@@ -18,25 +22,5 @@ RSpec.describe Task, type: :model do
   it 'contentが空であればバリデーションが通らない' do
     task = Task.new(title: 'utumukanaide', content: '')
     expect(task).not_to be_valid
-  end
-
-  it 'titleとcontentが記載されていればバリテーションが通る' do
-    task = Task.new(title: 'kitto', content: 'a-a-lov')
-    expect(task).to be_valid
-  end
-
-  it '未着手で検索できる' do
-    task = Task.not_yet_started
-    expect(task.pluck(:title)[0]).to eq 'not_yet_started_title'
-  end
-
-  it '開始で検索できる' do
-    task = Task.start
-    expect(task.pluck(:title)[0]).to eq 'start_title'
-  end
-
-  it '完了で検索できる' do
-    task = Task.complete
-    expect(task.pluck(:title)[0]).to eq 'complete_title'
   end
 end
