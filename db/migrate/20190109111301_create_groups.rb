118a65d1dc6b7b0e8b89ef5fae586d81266bd969
ponpppher <genmaier@gmail.com>
2019-01-19 15:38:57 +0900

    del group column in group table

--- db/migrate/20190109111301_create_groups.rb
+++ db/migrate/20190109111301_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
       t.string :name, null: false, unique: true
-      t.integer :groups, :owner_id, null: false
+      t.integer :owner_id, null: false
       t.timestamps
     end
     add_index :groups, :owner_id
